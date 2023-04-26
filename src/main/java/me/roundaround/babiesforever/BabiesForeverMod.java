67d35b69d5c912731456e9ecb9a9451560c9ac31
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-04-26 17:26:50 -0400

    Update build scripts, added tadpoles

--- src/main/java/me/roundaround/babiesforever/BabiesForeverMod.java
+++ src/main/java/me/roundaround/babiesforever/BabiesForeverMod.java
@@ -1,9 +1,13 @@
 package me.roundaround.babiesforever;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
+
+import java.util.Locale;
 
 public final class BabiesForeverMod implements ModInitializer {
   public static final String MOD_ID = "babiesforever";
@@ -11,5 +15,13 @@ public final class BabiesForeverMod implements ModInitializer {
 
   @Override
   public void onInitialize() {
+  }
+
+  public static boolean isNameBypass(Entity entity) {
+    if (!entity.hasCustomName()) {
+      return false;
+    }
+    String name = Formatting.strip(entity.getName().getString()).toLowerCase(Locale.ROOT);
+    return "growup".equals(name) || "grow up".equals(name) || "grow_up".equals(name);
   }
 }
--- src/main/java/me/roundaround/babiesforever/mixin/PassiveEntityMixin.java
+++ src/main/java/me/roundaround/babiesforever/mixin/PassiveEntityMixin.java
@@ -1,16 +1,12 @@
 package me.roundaround.babiesforever.mixin;
 
-import java.util.Locale;
-
+import me.roundaround.babiesforever.BabiesForeverMod;
+import net.minecraft.entity.passive.PassiveEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.PassiveEntity;
-import net.minecraft.util.Formatting;
 
 @Mixin(PassiveEntity.class)
 public abstract class PassiveEntityMixin {
@@ -20,20 +16,10 @@ public abstract class PassiveEntityMixin {
   @Inject(method = "setBreedingAge", at = @At(value = "HEAD"), cancellable = true)
   public void setBreedingAge(int age, CallbackInfo info) {
     PassiveEntity self = ((PassiveEntity) (Object) this);
-    if (self.getBreedingAge() < 0
-        && self.hasCustomName()
-        && !isNameBypass(self)
-        && age >= 0) {
-      breedingAge = -1;
+    if (self.getBreedingAge() < 0 && self.hasCustomName() && !BabiesForeverMod.isNameBypass(self) &&
+        age >= 0) {
+      this.breedingAge = -1;
       info.cancel();
     }
-  }
-
-  private static boolean isNameBypass(Entity entity) {
-    if (!entity.hasCustomName()) {
-      return false;
-    }
-    String name = Formatting.strip(entity.getName().getString()).toLowerCase(Locale.ROOT);
-    return "growup".equals(name) || "grow up".equals(name) || "grow_up".equals(name);
   }
 }
